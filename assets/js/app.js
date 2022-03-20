8d1207d52fd25de0f8249d2b42a7873be835fb51
w <wvvwvvvvwvvw@gmail.com>
2022-03-20 05:59:39 -0700

    delayed loading bar

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,22 @@ import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
+// Show progress bar on live navigation and form submits
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
+
+let topBarScheduled = undefined
+window.addEventListener("phx:page-loading-start", () => {
+  if (!topBarScheduled) {
+    topBarScheduled = setTimeout(() => topbar.show(), 120)
+  }
+})
+
+window.addEventListener("phx:page-loading-stop", () => {
+  clearTimeout(topBarScheduled)
+  topBarScheduled = undefined
+  topbar.hide()
+})
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 
 const hooks = {
@@ -18,11 +34,6 @@ const hooks = {
 }
 
 let liveSocket = new LiveSocket("/live", Socket, { hooks: hooks, params: { _csrf_token: csrfToken } })
-
-// Show progress bar on live navigation and form submits
-topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
-window.addEventListener("phx:page-loading-start", info => topbar.show())
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
