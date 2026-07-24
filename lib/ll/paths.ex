2a70025f80b2e11e68b763bf56b0f46c415e6401
w <wvvwvvvvwvvw@gmail.com>
2026-07-24 13:27:39 -0700

    Downcase paths

--- lib/ll/paths.ex
+++ lib/ll/paths.ex
@@ -4,26 +4,31 @@ defmodule LL.Paths do
   def root(), do: Application.get_env(:ll, :downloads_root)
 
   def get(%Series{} = series) do
-    name = String.replace(series.title, ~r/[^ a-zA-Z0-9\.\-\_]/, "") |> String.trim()
-    name = "#{name}-#{series.source.lang}-#{series.source.name}-#{series.id}"
+    name =
+      "#{series.title}-#{series.source.lang}-#{series.source.name}-#{series.id}"
+      |> String.replace(~r/[^ a-zA-Z0-9\.\-\_]/, "")
+      |> String.trim()
+
     Path.expand(root()) |> Path.join(name)
   end
 
   def get(%Chapter{} = chapter) do
-    title = String.replace(chapter.title, ~r/[^ a-zA-Z0-9\.\-\_]/, "") |> String.trim()
-
     name =
       if chapter.number > 0 do
-        "#{chapter.number}-#{title}"
+        "#{chapter.number}-#{chapter.title}"
       else
-        title
+        chapter.title
       end
 
-    name = "#{name}-#{chapter.scanlator}-#{chapter.id}"
+    name =
+      "#{name}-#{chapter.scanlator}-#{chapter.id}"
+      |> String.replace(~r/[^ a-zA-Z0-9\.\-\_]/, "")
+      |> String.trim()
 
     Repo.get(Series, chapter.series_id)
     |> Repo.preload(:source)
     |> get()
     |> Path.join(name)
+    |> String.downcase()
   end
 end
